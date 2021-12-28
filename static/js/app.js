9d367d5b705cf7f822b78a398d9067727b6760dc
mandie021-moEqdO <mandie021@outlook.com>
2021-12-28 17:27:43 -0500

    12/28

--- static/js/app.js
+++ static/js/app.js
@@ -1,59 +1,59 @@
 // fetch the json file
 async function main() {
-    const response = await fetch("./samples.json");
+    const response = await fetch("data/samples.json");
     const data = await response.json();
     console.log(data);
 
     let samples = data["samples"];
-    console.log('samples:', samples);
+    // console.log('samples:', samples);
 
+// Samples person for first person id
+    let first_id = samples[0];
+    // console.log(first_id);
+
+/// getting some variables for the first samples 
+    var otu_labels = first_id.otu_labels;
+    var otu_ids = first_id.otu_ids;
+    var sample_values = first_id.sample_values;    
+    // console.log(otu_labels);
+    // console.log(otu_ids);
+    // console.log(sample_values);
+
+// slicing and sorting
+    var topten_values = sample_values.sort((a,b) => b - a).slice(0,10).reverse();
+    var top_labels = otu_labels.slice(0,10);
+    var top_ids = otu_ids.slice(0,10).map(thing => `OTU ${thing}`).reverse();
+ 
+    
 /// BAR CHART 
 
-
-    for (let i = 0; i < samples.length; i++) {    
-        var samplesArray = samples[i];         
-        // console.log(samplesArray);
-        // create array of each item values
-        var otu_labels = Object.values(samplesArray.otu_labels);
-        var otu_ids = Object.values(samplesArray.otu_ids);
-        var sample_values = Object.values(samplesArray.sample_values);
-    }
-    // Sort and get top 10 otu_ids
-    let sortedByotuid = samples.sort((a, b) => b.otu_ids - a.otu_ids);
-    otu_top10 = sortedByotuid.slice(1,10);
-        // Create our first trace
+        // Create barchart
     var barchart = [{
-        x: sample_values,
-        y: otu_ids,
-        // name: [`OTU` + {otu_ids}],
+        x: topten_values,
+        y: top_ids,
         type: "bar",
-        text: otu_labels,
-        orientation: "h",
-        
+        text: top_labels,
+        orientation: "h",   
     }];
     console.log("barchart data", barchart)
+
     // layout to apply title to chart
     let layout1 = { 
-        title: "Top Ten Samples",
-       xaxis: {
-           title: "Sample Values",
-       },
-       yaxis: {
-           title: [`OTU ${otu_ids}`],
-       },
-    
+        title: "Top Ten Samples",    
     }
+
     // plot bar chart and layout
     Plotly.newPlot("bar", barchart, layout1, {responsive: true}); 
-            
 
-        
+
 /// BUBBLE CHART
    
+        // Create bubblechart
     var bubble = [{
         x: otu_ids,
         y: sample_values,
         text: otu_labels,
+        type: 'scatter',
         mode: 'markers',
         marker: {
             size: sample_values,
@@ -63,23 +63,78 @@ async function main() {
     }];
 
     console.log("bubblechart data", bubble);
+
     // layout to apply sizing and title
     let layout2 = {
-        title: "Values of Samples",
+        title: "Bacteria Per Sample",
         xaxis: {
             title: "OTU ID",
         },
-        yaxis: {
-            title: "Counts"
-        },
     }
+
     // plot bubble chart and layout
     Plotly.newPlot("bubble", bubble, layout2, {responsive: true});
 
 
+/// Drop down with names
+    var dropdown = document.getElementById('selDataset')
+    var names = data.names
+    // console.log(names)
+    for(var i = 0; i < names.length; i++) {
+        var id_names = names[i];
+        var el = document.createElement("option");
+        el.textContent = id_names;
+        el.value = id_names;
+        dropdown.appendChild(el);
+    }
+
 /// DEMOGRAPHIC PANEL
-    let metadata = data.metadata; 
-    console.log("metadata", metadata) 
+    var PANEL = document.getElementById('sample-metadata')
+    let metadata = data.metadata
+    let first_info = metadata[0];
+
+    // for(var i = 0; i < metadata.length; i++) {
+        for(const [key, value] of Object.entries(first_info)) {
+            console.log(`${key.toUpperCase()}: ${value}`);
+            // var el = document.createElement("option");
+            // el.textContent = info;
+            // el.value = info;
+            PANEL.append(`${key.toUpperCase()}: ${value}`);
+        };
+    // }    
+   
+    // key value pair panel
+    
+        
+
+    // var sample-metadata
+
+    //     document.querySelector("#selDataset").addEventListener("change", event => {
+    //         for (let i = 0; i < metadata.length; i++) {    
+    //                 let selDataset = metadata[i];         
+                    
+    //                 // create array of each item values
+    //                 // var otu_labels = Object.values(samplesArray.otu_labels);
+    //                 // var otu_ids = Object.values(samplesArray.otu_ids);
+    //                 // var sample_values = Object.values(samplesArray.sample_values);        
+    //         };
+        
+    //     $('#selDataset').hide();
+    //     optionChanged();
+    // });
+
+/// DASHBOARD SAMPLE CHANGE
+    const optionChanged = async function () {
+        console.log(selDataset());        
+    }
+
+    // const
+        
+
+    
+    
+    
+
 
     
 
@@ -89,6 +144,5 @@ async function main() {
    
 
 }
-
 
 main();
