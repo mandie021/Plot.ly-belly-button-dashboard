431a6cf086b3af8f891007bc90166930e87b4751
mandie021-moEqdO <mandie021@outlook.com>
2022-01-03 17:22:52 -0500

    added eventlistener

--- static/js/app.js
+++ static/js/app.js
@@ -4,13 +4,16 @@ async function main() {
     const data = await response.json();
     console.log(data);
 
-    let samples = data["samples"];
+    // let samples = data["samples"];
 
     // console.log('samples:', samples);
 
 /// Drop down with names
     /// using data.names for the ID #
 var dropdown = document.getElementById('selDataset');
+dropdown.addEventListener('click', function(event) { 
+    sample = (event.target.value) 
+}) 
 var names = data.names;
 for(var i = 0; i < names.length; i++) {
     var id_names = names[i];
@@ -18,8 +21,8 @@ for(var i = 0; i < names.length; i++) {
     id.textContent = id_names;
     id.value = id_names;
     dropdown.appendChild(id);
-    console.log("ID", id)
-    console.log("names", id_names)
+    // console.log("ID", id)
+    // console.log("names", id_names)
 };
 
 
@@ -43,40 +46,35 @@ main();
 /// BAR & BUBBLE CHART 
 // variables for the chart build
     //using data.samples for the sample values, otu ids, and otu labels
-    function chartBuild(new_id){
+    async function chartBuild(new_id){
+        const response = await fetch("data/samples.json");
+        const data = await response.json();
+        var dropdown = document.getElementById('selDataset');
+        dropdown.addEventListener('click', function(event) { 
+            sample = (event.target.value) 
+        }) 
+        // console.log(data);
         let samples = data.samples;
-        var selected_sample;
+ 
     // Loop through the length of the array
         for (let i = 0; i < samples.length; i++) {
             console.log (i, samples[i]);
-            if (samples[i].id == id_names) {
+            if (samples[i].id == sample) {
                 new_id = samples[0];
                 console.log(new_id)
             }
         };
-        // const sArray = Object.entries(samples);
-        
-
-        // let sArray = samples.filter(samples => samples.id == id.names);
-//         console.log(samples) 
-            // console.log(sArray)
-//         let selected_sample = sArray[0];
-//         console.log(selected_sample)
-
 
 // bubble chart values
-        var otu_labels = selected_sample.otu_labels;
-        var otu_ids = selected_sample.otu_ids;
-        var sample_values = selected_sample.sample_values;   
+        var otu_labels = new_id.otu_labels;
+        var otu_ids = new_id.otu_ids;
+        var sample_values = new_id.sample_values;   
 
         
 // slicing and sorting for bar chart
         var topten_values = sample_values.sort((a,b) => b - a).slice(0,10).reverse();
         var top_labels = otu_labels.slice(0,10);
         var top_ids = otu_ids.slice(0,10).map(thing => `OTU ${thing}`).reverse();
-
-   
-
 
 // Create barchart
     var barchart = [{
@@ -91,7 +89,7 @@ main();
     // layout to apply title to chart
     let layout1 = { 
         title: "Top Ten Samples",    
-    }
+    };
 
     // plot bar chart and layout
     Plotly.newPlot("bar", barchart, layout1, {responsive: true}); 
@@ -119,27 +117,36 @@ main();
         xaxis: {
             title: "OTU ID",
         },
-    }
+    };
 
     // plot bubble chart and layout
     Plotly.newPlot("bubble", bubble, layout2, {responsive: true});
 
  };
-
+ chartBuild();
 
 /// DEMOGRAPHIC PANEL
-    function metaBuild(new_id){
+    async function metaBuild(new_id){
+        const response = await fetch("data/samples.json");
+        const data = await response.json();
+        var dropdown = document.getElementById('selDataset');
+        dropdown.addEventListener('click', function(event) { 
+            sample = (event.target.value) 
+        }) 
+        // console.log(data);
       var panel = document.getElementById('sample-metadata')
         let metadata = data.metadata
         for(let i = 0; i < metadata.length; i++){ 
-            mArray= metadata[i];
+            if (metadata[i].id == sample) {
+                meta_id = metadata[0];
+                console.log(meta_id)
+            }
         };      
-        let selcected_demo = mArray.filter == id_names;  
-        for(const [key, value] of Object.entries(selcected_demo)) {
+
+        for(const [key, value] of Object.entries(meta_id)) {
             panel.append(`${key.toUpperCase()}: ${value}`);
         };
     };  
 
-// };
 
-// main();
+metaBuild();
