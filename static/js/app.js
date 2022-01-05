87dfdfa53c3053bba9643f1ee1f31eaa1c7e5d10
mandie021-moEqdO <mandie021@outlook.com>
2022-01-05 14:54:05 -0500

    1/5

--- static/js/app.js
+++ static/js/app.js
@@ -20,19 +20,99 @@ for(var i = 0; i < names.length; i++) {
     id.value = id_names;
     dropdown.appendChild(id);
     // console.log("ID", id)
-    console.log("names", id_names)
+    // console.log("names", id_names)
+};
+let samples = data.samples;
+first_id = samples[0];
+
+// bubble chart values
+var otu_labels = first_id.otu_labels;
+var otu_ids = first_id.otu_ids;
+var sample_values = first_id.sample_values;   
+
+
+// slicing and sorting for bar chart
+var topten_values = sample_values.sort((a,b) => b - a).slice(0,10).reverse();
+var top_labels = otu_labels.slice(0,10);
+var top_ids = otu_ids.slice(0,10).map(thing => `OTU ${thing}`).reverse();
+
+// Create barchart
+var barchart = [{
+x: topten_values,
+y: top_ids,
+type: "bar",
+text: top_labels,
+orientation: "h",   
+}];
+// console.log("barchart data", barchart)
+
+// layout to apply title to chart
+let layout1 = { 
+title: "Top Ten Samples",    
 };
 
+// plot bar chart and layout
+Plotly.newPlot("bar", barchart, layout1, {responsive: true}); 
 
+// Create bubblechart
+var bubble = [{
+    x: otu_ids,
+    y: sample_values,
+    text: otu_labels,
+    type: 'scatter',
+    mode: 'markers',
+    marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "Earth",
+    }
+}];
+
+// console.log("bubblechart data", bubble);
+
+// layout to apply sizing and title
+let layout2 = {
+    title: "Bacteria Per Sample",
+    xaxis: {
+        title: "OTU ID",
+    },
+};
+
+// plot bubble chart and layout
+Plotly.newPlot("bubble", bubble, layout2, {responsive: true});
+
+/// DEMOGRAPHIC PANEL
+ 
+var panel = document.getElementById('sample-metadata')
+let metadata = data.metadata
+meta_id = metadata[0];
+        
+// display using h6 tag 
+for(const [key, value] of Object.entries(meta_id)) {
+    var h6 =  document.createElement("h6");
+    panel.append(h6,`${key.toUpperCase()}: ${value}`);
+};
+
+// console.log(id)
 // Samples person for first person id
     /// using data.names ID 940 
-    let first_id = names[0];
-    console.log("first id", first_id);
-    chartBuild(first_id);
-    metaBuild(first_id);
+    // let first_id = names[0];
+    // // console.log("first id", first_id);
+    // chartBuild(first_id);
+    // metaBuild(first_id);
 };
+
+
 //START DASHBOARD
+//by closing main function
 main();
+
+//EVENT LISTENER
+//to get new id #
+    var new_id = document.addEventListener("change", event => {
+        document.querySelector("option").value;
+    });
+    console.log(new_id)
 
 /// DASHBOARD SAMPLE CHANGE
     /// for new id #
@@ -40,7 +120,7 @@ main();
         chartBuild(new_id);
         metaBuild(new_id);
     };
-    
+        
 /// BAR & BUBBLE CHART 
 // variables for the chart build
     //using data.samples for the sample values, otu ids, and otu labels
@@ -49,21 +129,20 @@ main();
         const data = await response.json();
         // console.log(data);
         let samples = data.samples;
-        first_id = samples[0];
+    
   
+        var resultArray = samples.filter(sampleObj => sampleObj.id == new_id);
+        var new_sample= resultArray[0]
+
     // Loop through the length of the array
-        for (let i = 0; i < samples.length; i++) {
-            console.log (i, samples[i]);
-            if (samples[i].id == new_id) {
-                new_id = samples[0];
-                console.log(new_id)
-            }
-        };
+
+   
+
 
 // bubble chart values
-        var otu_labels = new_id.otu_labels;
-        var otu_ids = new_id.otu_ids;
-        var sample_values = new_id.sample_values;   
+        var otu_labels = new_sample.otu_labels;
+        var otu_ids = new_sample.otu_ids;
+        var sample_values = new_sample.sample_values;   
 
         
 // slicing and sorting for bar chart
@@ -79,7 +158,7 @@ main();
         text: top_labels,
         orientation: "h",   
     }];
-    console.log("barchart data", barchart)
+    // console.log("barchart data", barchart)
 
     // layout to apply title to chart
     let layout1 = { 
@@ -104,7 +183,7 @@ main();
         }
     }];
 
-    console.log("bubblechart data", bubble);
+    // console.log("bubblechart data", bubble);
 
     // layout to apply sizing and title
     let layout2 = {
@@ -121,7 +200,7 @@ main();
  chartBuild();
 
 /// DEMOGRAPHIC PANEL
-    async function metaBuild(new_id){
+    async function metaBuild(any_id){
         const response = await fetch("data/samples.json");
         const data = await response.json();
         
@@ -130,18 +209,16 @@ main();
         let metadata = data.metadata
         for(let i = 0; i < metadata.length; i++){ 
             if (metadata[i].id == new_id) {
-                new_id = metadata[0];
-                // console.log(meta_id)
+                // new_id = metadata[0];
             }
         };      
 
-        // clear the data of exsisting
-        panel.innerHTML = "";
+        // // clear the data of exsisting
+        // panel.innerHTML = "";
 
         // display using h6 tag 
         for(const [key, value] of Object.entries(new_id)) {
             var h6 =  document.createElement("h6");
-
             panel.append(h6,`${key.toUpperCase()}: ${value}`);
         };
     };  
