edd897e9293e5bb2bfe07e3fc1328a791e818bad
mandie021-moEqdO <mandie021@outlook.com>
2022-01-05 18:38:37 -0500

    final push

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 // fetch the json file
 async function main() {
     const response = await fetch("data/samples.json");
-    const data = await response.json();
+    data = await response.json();
     // console.log(data);
 
     /// Drop down with names
@@ -18,89 +18,14 @@ async function main() {
         // console.log("ID", id)
         // console.log("names", id_names)
     };
-    let samples = data.samples;
-    first_id = samples[0];
 
-    // bubble chart values
-    var otu_labels = first_id.otu_labels;
-    var otu_ids = first_id.otu_ids;
-    var sample_values = first_id.sample_values;   
-
-
-    // slicing and sorting for bar chart
-    var topten_values = sample_values.sort((a,b) => b - a).slice(0,10).reverse();
-    var top_labels = otu_labels.slice(0,10);
-    var top_ids = otu_ids.slice(0,10).map(thing => `OTU ${thing}`).reverse();
-
-    // Create barchart
-    var barchart = [{
-    x: topten_values,
-    y: top_ids,
-    type: "bar",
-    text: top_labels,
-    orientation: "h",   
-    }];
-    // console.log("barchart data", barchart)
-
-    // layout to apply title to chart
-    let layout1 = { 
-    title: "Top Ten Samples",    
-    };
-
-    // plot bar chart and layout
-    Plotly.newPlot("bar", barchart, layout1, {responsive: true}); 
-
-    // Create bubblechart
-    var bubble = [{
-        x: otu_ids,
-        y: sample_values,
-        text: otu_labels,
-        type: 'scatter',
-        mode: 'markers',
-        marker: {
-            size: sample_values,
-            color: otu_ids,
-            colorscale: "Earth",
-        }
-    }];
-
-    // console.log("bubblechart data", bubble);
-
-    // layout to apply sizing and title
-    let layout2 = {
-        title: "Bacteria Per Sample",
-        xaxis: {
-            title: "OTU ID",
-        },
-    };
-
-    // plot bubble chart and layout
-    Plotly.newPlot("bubble", bubble, layout2, {responsive: true});
-
-    /// DEMOGRAPHIC PANEL
-    var panel = document.getElementById('sample-metadata');
-    let metadata = data.metadata;
-    meta_id = metadata[0];
-            
-    // display using h6 tag 
-    for(const [key, value] of Object.entries(meta_id)) {
-        var h6 =  document.createElement("h6");
-        panel.append(h6,`${key.toUpperCase()}: ${value}`);
-    };
+    // Set the inital value for the select box
+    dropdown.value = data.names[0];
+    chartBuild(dropdown.value);
+    metaBuild(dropdown.value);
 
 };
 
-
-//START DASHBOARD
-//by closing main function
-main();
-
-//EVENT LISTENER
-//to get new id #
-var new_id = document.addEventListener("click", event => {
-    document.querySelector("option").value;
-});
-// console.log(new_id)
 
 /// DASHBOARD SAMPLE CHANGE
 /// for new id #
@@ -113,9 +38,7 @@ function optionChanged(new_id) {
 // variables for the chart build
     //using data.samples for the sample values, otu ids, and otu labels
 async function chartBuild(new_id){
-    const response = await fetch("data/samples.json");
-    const data = await response.json();
-    // console.log(data);
+
     let samples = data.samples;
     var resultArray = samples.filter(sampleObj => sampleObj.id == new_id);
     var new_sample= resultArray[0]
@@ -178,12 +101,12 @@ async function chartBuild(new_id){
     Plotly.newPlot("bubble", bubble, layout2, {responsive: true});
 
  };
- chartBuild();
+
 
 /// DEMOGRAPHIC PANEL
 async function metaBuild(new_id){
-    const response = await fetch("data/samples.json");
-    const data = await response.json();
+    // const response = await fetch("data/samples.json");
+    // const data = await response.json();
         
     // console.log(data);
     var panel = document.getElementById('sample-metadata');
@@ -200,4 +123,6 @@ async function metaBuild(new_id){
         panel.append(h6,`${key.toUpperCase()}: ${value}`);
     };
 };  
-metaBuild();
+
+let data = {}
+main();
